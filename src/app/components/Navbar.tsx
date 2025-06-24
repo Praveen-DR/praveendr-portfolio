4a30f07ab3b64fd41a610ccff3dfc7669e56a4b0
Praveen D R <praveendr07@gmail.com>
2025-06-24 12:25:47 +0530

    made some changes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { MoonIcon, SunIcon } from '@heroicons/react/16/solid';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-import { button } from 'motion/react-client';
+import { button, div } from 'motion/react-client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React, { useState } from 'react'
@@ -22,12 +22,12 @@ const Navbar = () => {
         { href: "/contact", lable: "Contact" },
     ]
     return (
-        <>
-            <nav className='fixed w-full dark:bg-dark/80 backdrop-blur-sm z-50'>
+            <nav className='fixed w-full bg-dark/80 backdrop-blur-sm z-50'>
                 <div className='container max-w-7xl mx-auto px-4'>
                     {/* {desktop menu} */}
                     <div className='flex items-center justify-between h-16'>
                         <Link href="/" className="text-xl font-bold text-primary">Portfolio&trade;</Link>
+                        
                         {/* {desktop menus} */}
                         <div className='hidden md:flex items-center space-x-8'>
                             {
@@ -38,28 +38,26 @@ const Navbar = () => {
                                             transition-colors font-medium ${isActive ? ' text-primary' : ''}`}>{item.lable}
                                         </Link>
                                     )
-
                                 })
-                                
                             }
                             <button className='p-2 rounded-lg hover:bg-gray-100 text-primary dark:hover:bg-gray-800 transition-colors cursor-pointer'>
                                 {
                                     theme === "dark" ? (
-                                        <SunIcon className='w-5 h-5'/>
+                                        <SunIcon className='w-5 h-5' />
                                     ) : (
-                                        <MoonIcon className='w-5 h-5'/>
+                                        <MoonIcon className='w-5 h-5' />
                                     )
                                 }
                             </button>
                         </div>
-                    </div>
+                    
 
                     {/* {mobile view} */}
                     <button
-                    onClick={toggleMobileMenu} 
-                    className='md:hidden p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors cursor-pointer'>
+                        onClick={toggleMobileMenu}
+                        className='md:hidden p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors cursor-pointer'>
                         {
-                            isMobileMenuOpen ? (<XMarkIcon className='w-6 h-6'/>) : (<Bars3Icon className='w-6 h-6'/>)
+                            isMobileMenuOpen ? (<XMarkIcon className='w-6 h-6' />) : (<Bars3Icon className='w-6 h-6' />)
                         }
                     </button>
                 </div>
@@ -67,12 +65,37 @@ const Navbar = () => {
 
                 {/* {mobile menu} */}
                 {
-                    
-                }
+                    isMobileMenuOpen && (
+                        <div className='md-hidden'>
+                            <div className='py-4 space-y-4'>
+                                {
+                                    menuItems.map((item, index) => (
+                                        <div key={index} onClick={toggleMobileMenu} >
+                                            <Link  href={item.href} className='block py-2 hover:text-primary transition-colors'>{item.lable}</Link>
+                                        </div>
+                                    ))
+                                }
 
+                                <div>
+                                    <button className='flex items-center py-2 hover: text-primary transition-colors'>
+                                {
+                                    theme === "dark" ? (
+                                        <><SunIcon className='w-5 h-5 mr-2' /> Light Mode</>
+                                    ) : (
+                                        <><MoonIcon className='w-5 h-5 mr-2' /> Dark Mode</>
+                                    )
+                                }
+                            </button>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    )
+                }
+            </div>
             </nav >
-        </>
     )
-} 
+}
 
 export default Navbar
