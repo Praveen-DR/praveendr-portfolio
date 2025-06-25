7b100ee3c9e13189594ffd41bccf7093ae2cb500
Praveen D R <praveendr07@gmail.com>
2025-06-26 02:01:24 +0530

    update:added bio section and About me

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>about</div>
+    <div className='container max-w-7xl mx-auto py-20'>
+      <h1 className='text-4xl font-bold mb-8 text-center'>About Me</h1>
+      
+      
+      
+      {/* bio section */}
+      <section className='mb-16'>
+        <p className='text-lg text-secondary max-w-3xl mx-auto text-center'>I'm a passionate Full Stack Developer with expertise in building modern web applications.
+           With a strong foundation in both frontend and backend technologies, 
+           I create seamless user experiences and robust server-side solutions.
+        </p>
+      </section>
+
+
+    
+    </div>
   )
 }
 
