0418d0f7be26968af64853306506289867648d0a
Praveen D R <praveendr07@gmail.com>
2025-06-25 12:58:29 +0530

    made sone changes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -22,35 +22,35 @@ const Navbar = () => {
         { href: "/contact", lable: "Contact" },
     ]
     return (
-            <nav className='fixed w-full bg-dark/80 backdrop-blur-sm z-50'>
-                <div className='container max-w-7xl mx-auto px-4'>
-                    {/* {desktop menu} */}
-                    <div className='flex items-center justify-between h-16'>
-                        <Link href="/" className="text-xl font-bold text-primary">Portfolio&trade;</Link>
-                        
-                        {/* {desktop menus} */}
-                        <div className='hidden md:flex items-center space-x-8'>
-                            {
-                                menuItems.map((item) => {
-                                    const isActive = pathName === item.href;
-                                    return (
-                                        <Link key={item.href} href={item.href} className={`hover:text-primary 
+        <nav className='fixed w-full bg-dark/80 backdrop-blur-sm z-50'>
+            <div className='container max-w-7xl mx-auto px-4'>
+                {/* {desktop menu} */}
+                <div className='flex items-center justify-between h-16'>
+                    <Link href="/" className="text-xl font-bold text-primary">Portfolio&trade;</Link>
+
+                    {/* {desktop menus} */}
+                    <div className='hidden md:flex items-center space-x-8'>
+                        {
+                            menuItems.map((item) => {
+                                const isActive = pathName === item.href;
+                                return (
+                                    <Link key={item.href} href={item.href} className={`hover:text-primary 
                                             transition-colors font-medium ${isActive ? ' text-primary' : ''}`}>{item.lable}
-                                        </Link>
-                                    )
-                                })
+                                    </Link>
+                                )
+                            })
+                        }
+                        <button className='p-2 rounded-lg hover:bg-gray-100 text-white hover:text-primary dark:hover:bg-gray-800 transition-colors cursor-pointer'>
+                            {
+                                theme === "dark" ? (
+                                    <SunIcon className='w-5 h-5' />
+                                ) : (
+                                    <MoonIcon className='w-5 h-5' />
+                                )
                             }
-                            <button className='p-2 rounded-lg hover:bg-gray-100 text-primary dark:hover:bg-gray-800 transition-colors cursor-pointer'>
-                                {
-                                    theme === "dark" ? (
-                                        <SunIcon className='w-5 h-5' />
-                                    ) : (
-                                        <MoonIcon className='w-5 h-5' />
-                                    )
-                                }
-                            </button>
-                        </div>
-                    
+                        </button>
+                    </div>
+
 
                     {/* {mobile view} */}
                     <button
@@ -71,21 +71,21 @@ const Navbar = () => {
                                 {
                                     menuItems.map((item, index) => (
                                         <div key={index} onClick={toggleMobileMenu} >
-                                            <Link  href={item.href} className='block py-2 hover:text-primary transition-colors'>{item.lable}</Link>
+                                            <Link href={item.href} className='block py-2 hover:text-primary transition-colors'>{item.lable}</Link>
                                         </div>
                                     ))
                                 }
 
                                 <div>
                                     <button className='flex items-center py-2 hover: text-primary transition-colors'>
-                                {
-                                    theme === "dark" ? (
-                                        <><SunIcon className='w-5 h-5 mr-2' /> Light Mode</>
-                                    ) : (
-                                        <><MoonIcon className='w-5 h-5 mr-2' /> Dark Mode</>
-                                    )
-                                }
-                            </button>
+                                        {
+                                            theme === "dark" ? (
+                                                <><SunIcon className='w-5 h-5 mr-2' /> Light Mode</>
+                                            ) : (
+                                                <><MoonIcon className='w-5 h-5 mr-2' /> Dark Mode</>
+                                            )
+                                        }
+                                    </button>
                                 </div>
 
                             </div>
@@ -94,7 +94,7 @@ const Navbar = () => {
                     )
                 }
             </div>
-            </nav >
+        </nav >
     )
 }
 
