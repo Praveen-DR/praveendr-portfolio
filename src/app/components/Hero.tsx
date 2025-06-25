1ec1b280575b617d33b10e68c23f7fa7999a9f23
Praveen D R <praveendr07@gmail.com>
2025-06-25 23:49:57 +0530

    Update: profile name changes

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <section className="py-28 container max-w-7xl mx-auto px-4">
         <div className="max-w-3xl mx-auto text-center">
           <div className="flex flex-col items-center mb-4">
-            <Image src="/Profile.jpg" alt="Profile" width={100} height={100} className="rounded-full mb-4 w-32 h-32 object-cover 
+            <Image src="/Profile.JPG" alt="Profile" width={100} height={100} className="rounded-full mb-4 w-32 h-32 object-cover 
             ring-2 ring-primary" />
           </div>
 
