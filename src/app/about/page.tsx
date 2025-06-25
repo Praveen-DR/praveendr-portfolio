c1cf2620cb79b3cabeb1bfc06cfe591ebf28c341
Praveen D R <praveendr07@gmail.com>
2025-06-26 02:18:03 +0530

    update:added front end and backend section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,22 +1,51 @@
 import React from 'react'
+import { FaCode, FaLaptop } from 'react-icons/fa'
 
 const About = () => {
   return (
     <div className='container max-w-7xl mx-auto py-20'>
       <h1 className='text-4xl font-bold mb-8 text-center'>About Me</h1>
-      
-      
-      
+
+
+
       {/* bio section */}
       <section className='mb-16'>
         <p className='text-lg text-secondary max-w-3xl mx-auto text-center'>I'm a passionate Full Stack Developer with expertise in building modern web applications.
-           With a strong foundation in both frontend and backend technologies, 
-           I create seamless user experiences and robust server-side solutions.
+          With a strong foundation in both frontend and backend technologies,
+          I create seamless user experiences and robust server-side solutions.
         </p>
       </section>
 
+      {/* skills section */}
+      <section className='mb-16'>
+        <h2 className='section-title'>Skills</h2>
+        <div className='grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+          <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md '>
+            <FaCode className='h-8 w-8 text-primary mb-4'/>
+            <h3 className='text-xl font-semibold mb-2'>Frontend</h3>
+            <ul className='text-secondary space-y-2'>
+              <li>React / next.js</li>
+              <li>TypeScript</li>
+              <li>Tailwind CSS</li>
+              <li>HTML/CSS</li>
+            </ul>
+          </div>
 
-    
+
+
+          <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md '>
+            <FaLaptop className='h-8 w-8 text-primary mb-4'/>
+            <h3 className='text-xl font-semibold mb-2'>Frontend</h3>
+            <ul className='text-secondary space-y-2'>
+              <li>Spring Boot</li>
+              <li>Java</li>
+              <li>PHP</li>
+              <li>MySQL</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
     </div>
   )
 }
