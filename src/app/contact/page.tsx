2426f75424591c4cc74391e037d2a3b5550a0130
Praveen D R <praveendr07@gmail.com>
2025-06-26 14:59:30 +0530

    update: added contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,63 @@
+"use client"
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { FaEnvelope, FaMapMarkerAlt, FaPhone } from 'react-icons/fa'
 
+interface FormData{
+  name: string;
+  email: string;
+  message: string;
+}
+
+type FormStatus = "idle"| "loading" | "success" | "error";
+
 const ContactPage = () => {
+  const [formData, setFormData] = useState<FormData>({
+    name:"",
+    email:"",
+    message:"",
+  });
+
+const [status, setStatus] = useState<FormStatus>("idle");
+
+const handleSubmit = async(e: React.FormEvent) => {
+    e.preventDefault();
+    setStatus("loading")
+    try{
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(formData)
+      })
+
+      if(!response.ok){
+        throw new Error("Failed to send message");
+      }
+
+      setStatus("success");
+      setFormData({
+        name:"",
+        email:"",
+        message:""
+      })
+    }catch(error){
+        setStatus("error");
+        console.error("Error sending message:", error);
+    }
+
+}
+
+const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  setFormData(prev => ({
+    ...prev,
+    [e.target.name]: e.target.value
+     
+  }))
+}
+
   return (
     <>
       <div className='container max-w-7xl mx-auto py-20'>
@@ -54,19 +109,27 @@ const ContactPage = () => {
 
           {/* contact form */}
           <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-lg'>
-          <form className='space-y-6'>
+          <form onSubmit={handleSubmit} className='space-y-6'>
               <div>
                 <label htmlFor="name" className='block text-sm font-medium mb-2'>Name</label>
-                <input type='text' id="name" name='name' placeholder='Enter you name'
-                className='w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-700
+                <input
+                required
+                value={formData.name}
+                onChange={handleChange}
+                type='text' id="name" name='name' placeholder='Enter you name'
+                className='w-full px-4 py-2 rounded-md border  border-gray-300 placeholder:text-white/50 dark:border-gray-700
                 bg-white dark:bg-dark focus:ring-2 focus:ring-primary focus:border-none'/>
               </div>
 
 
               <div>
                 <label htmlFor="email" className='block text-sm font-medium mb-2'>Email</label>
-                <input type='email' id="email" name="email" placeholder='Enter you email'
-                className='w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-700
+                <input 
+                required
+                value={formData.email}
+                onChange={handleChange}
+                type='email' id="email" name="email" placeholder='Enter you email'
+                className='w-full px-4 py-2 rounded-md border placeholder:text-white/50 border-gray-300 dark:border-gray-700
                 bg-white dark:bg-dark focus:ring-2 focus:ring-primary focus:border-none'/>
               </div>
 
@@ -74,11 +137,30 @@ const ContactPage = () => {
               
               <div>
                 <label htmlFor="message" className='block text-sm font-medium mb-2'>Message</label>
-                <textarea  id="message" name="message" placeholder='Enter you message'
-                className='w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-700
+                <textarea 
+                rows={4}
+                value={formData.message}
+                onChange={handleChange}
+                required
+                id="message" name="message" placeholder='Enter you message'
+                className='w-full px-4 py-2 rounded-md border border-gray-300 placeholder:text-white/50 dark:border-gray-700
                 bg-white dark:bg-dark focus:ring-2 focus:ring-primary focus:border-none'/>
               </div>
+              <button type='submit'className="w-full btn btn-primary">
+                {status === "loading" ? "sending..." : "Send Message"}
+              </button>
 
+              {
+                status === "success" && (
+                  <p className='text-green-500 text-center'>Message sent successfully!</p>
+                )
+              }
+
+              {
+                status === "error" && (
+                  <p className='text-red-500 text-center'>Failed to send Message. Please try again.!</p>
+                )
+              }
           </form>
 
 
