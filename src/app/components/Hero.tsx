588c4d896c982470b2a7938f48da18f7195ee2c2
Praveen D R <praveendr07@gmail.com>
2025-06-27 12:59:19 +0530

    Update: added resume button for hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -10,20 +10,22 @@ export default function Hero() {
   return (
     <section className="py-28">
       <div className="container max-w-7xl mx-auto px-4">
+
+
         <div className="max-w-3xl mx-auto text-center">
-          <motion.div 
+          <motion.div
             className='flex justify-center items-center mb-4'
             {...scaleIn}
             transition={{ delay: 0.2 }}
           >
             <Image src="/Profile.JPG" alt="Profile" width={100} height={100} className="rounded-full mb-4 w-32 h-32 object-cover ring-2 ring-primary" />
           </motion.div>
-          <motion.h1 
+          <motion.h1
             className="text-4xl md:text-6xl font-bold mb-6"
             {...fadeInUp}
             transition={{ delay: 0.3 }}
           >
-            Hi, I&apos;m <motion.span 
+            Hi, I&apos;m <motion.span
               className="text-primary"
               {...fadeIn}
               transition={{ delay: 0.8 }}
@@ -31,58 +33,69 @@ export default function Hero() {
               Praveen D R
             </motion.span>
           </motion.h1>
-          <motion.p 
+          <motion.p
             className="text-xl md:text-2xl text-gray-600 dark:text-gray-300 mb-8"
             {...fadeInUp}
             transition={{ delay: 0.4 }}
           >
-            Full Stack Developer | UI/UX Enthusiast 
+            Full Stack Developer | UI/UX Enthusiast
           </motion.p>
-          <motion.div 
-            className="flex justify-center space-x-4 mb-8"
-            {...fadeInUp}
-            transition={{ delay: 0.5 }}
-          >
-            <motion.a
-              href="https://github.com/Praveen-DR/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-2xl text-gray-600 hover:text-primary dark:text-gray-300 dark:hover:text-primary transition-colors"
-              whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 0.9 }}
+          <div className='flex-row'>
+            <motion.div
+              className="flex justify-center space-x-4 mb-8"
+              {...fadeInUp}
+              transition={{ delay: 0.5 }}
             >
-              <FaGithub />
-            </motion.a>
-            <motion.a
-              href="https://www.linkedin.com/in/praveen-d-r-201a24338/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-2xl text-gray-600 hover:text-primary dark:text-gray-300 dark:hover:text-primary transition-colors"
-              whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 0.9 }}
-            >
-              <FaLinkedin />
-            </motion.a>
-            <motion.a
-              href="https://www.instagram.com/smart_chain_stroker_/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-2xl text-gray-600 hover:text-primary dark:text-gray-300 dark:hover:text-primary transition-colors"
-              whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 0.9 }}
-            >
-              <FaInstagram />
-            </motion.a>
-          </motion.div>
-          <motion.div 
+              <motion.a
+                href="https://github.com/Praveen-DR/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-2xl text-gray-600 hover:text-primary dark:text-gray-300 dark:hover:text-primary transition-colors"
+                whileHover={{ scale: 1.2 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <FaGithub />
+              </motion.a>
+              <motion.a
+                href="https://www.linkedin.com/in/praveen-d-r-201a24338/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-2xl text-gray-600 hover:text-primary dark:text-gray-300 dark:hover:text-primary transition-colors"
+                whileHover={{ scale: 1.2 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <FaLinkedin />
+              </motion.a>
+              <motion.a
+                href="https://www.instagram.com/smart_chain_stroker_/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-2xl text-gray-600 hover:text-primary dark:text-gray-300 dark:hover:text-primary transition-colors"
+                whileHover={{ scale: 1.2 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <FaInstagram />
+
+
+              </motion.a>
+            </motion.div>
+          </div>
+
+          <motion.div
             className="flex flex-col md:flex-row justify-center gap-4"
             {...fadeInUp}
             transition={{ delay: 0.6 }}
           >
+            
+
+
             <motion.div
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
             >
+
+
+              
               <Link
                 href="/projects"
                 className="bg-primary inline-block w-full md:w-auto text-white px-8 py-3 rounded-lg hover:bg-primary/90 transition-colors"
@@ -100,11 +113,19 @@ export default function Hero() {
               >
                 Contact Me
               </Link>
-
-              
-
             </motion.div>
+
           </motion.div>
+
+          <motion.a
+              whileHover={{ scale: 1.1 }}
+              href="/Praveen_DR_Resume.pdf"
+              role="link"
+              className="mt-7 font-bold inline-flex items-center justify-center gap-2 px-4 py-2 text-sm text-white transition bg-gray-800 border border-gray-600 rounded-full hover:bg-gray-900 hover:border-gray-700 hover:text-white focus:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black"
+            >
+              Resume
+            </motion.a>
+
         </div>
       </div>
     </section>
--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -32,53 +32,22 @@ export const projects: Project[] = [
       },
 
       {
+        title: 'Poke Dex API',
+        description: 'RESTful API built using Spring Boot with a clean MVC architecture that displays all the Pokémon available along with their details.',
+        technologies: ['Spring Boot', 'Next.js', 'Tailwind CSS', 'JavaScript', 'MySQL'],
+        githubLink: 'https://github.com/Praveen-DR/pokedex-api',
+        demoLink: 'https://praveendr-portfolio.vercel.app/',
+        image: '/projects/pokemon.jpg',
+      },
+
+      {
         title: 'E-commerce Platform',
         description: 'A full-stack e-commerce platform built with Next.js, TypeScript, and Tailwind CSS.',
         technologies: ['Next.js', 'TypeScript', 'Tailwind CSS', 'Stripe'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
+        githubLink: 'https://github.com/Praveen-DR',
+        demoLink: 'https://praveendr-portfolio.vercel.app/',
         image: '/projects/e-commerce-website.png',
       },
-      {
-        title: 'Task Management App',
-        description: 'A collaborative task management application with real-time updates.',
-        technologies: ['React', 'Node.js', 'MongoDB', 'Socket.io'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/task-manager.webp',
-      },
       
-      {
-        title: 'Blog Platform',
-        description: 'A blogging platform with user authentication and markdown support.',
-        technologies: ['Gatsby', 'GraphQL', 'Contentful'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/blog-website.jpeg',
-      },
-      {
-        title: 'Weather App',
-        description: 'A weather application that provides real-time weather updates.',
-        technologies: ['React', 'OpenWeatherMap API'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/weather-app.png',
-      },
-      {
-        title: 'Chat Application',
-        description: 'A real-time chat application using WebSocket technology.',
-        technologies: ['React', 'Node.js', 'Socket.io'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/chat-app.png',
-      },
-      {
-        title: 'Recipe Finder',
-        description: 'A recipe search application using the Edamam API.',
-        technologies: ['React', 'Edamam API'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/recipe-finder.png',
-      },
       
   ];
