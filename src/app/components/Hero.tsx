731a6b84f731ef9073b32eba9537f46666f16e67
Praveen D R <praveendr07@gmail.com>
2025-06-28 22:04:42 +0530

    Update: added drive link to the resume

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -118,6 +118,7 @@ export default function Hero() {
             {...fadeInUp}
             transition={{ delay: 0.6 }}
           >
+
             <motion.div
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
@@ -146,7 +147,7 @@ export default function Hero() {
 
           <motion.a
             whileHover={{ scale: 1.1 }}
-            href="/Praveen_DR_Resume.pdf"
+            href="https://drive.google.com/file/d/1QlXADIaUtciKNoz4R0Zbm0VG5geGQ63r/view?usp=sharing"
             role="link"
             className="mt-7 font-bold inline-flex items-center justify-center gap-2 px-4 py-2 text-sm text-white transition bg-gray-800 border border-gray-600 rounded-full hover:bg-gray-900 hover:border-gray-700 hover:text-white focus:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black"
           >
