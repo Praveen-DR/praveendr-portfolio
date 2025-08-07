fd0d365ba41249750973a77f1f706eba410b7909
Praveen D R <praveendr07@gmail.com>
2025-08-07 17:47:14 +0530

    update: updated the resume

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -146,7 +146,7 @@ export default function Hero() {
           </motion.div>
 
           <motion.a
-            href="https://drive.google.com/file/d/1vca1twnX_7iT5FnKGS4iKWyXvLvitKqR/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Es_DwAgRBzhMfb7SP4nYO7__ZYhegCOZ/view?usp=sharing"
             role="link"
             className="mt-7 font-bold inline-flex items-center justify-center gap-2 px-4 py-2 text-sm text-white transition bg-gray-800 border border-gray-600 rounded-full hover:bg-gray-900 hover:border-gray-700 hover:text-white focus:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black"
         
